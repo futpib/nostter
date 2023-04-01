29b6c2dac451f3216874c9d62e009b02ce1c04ca
futpib <futpib@gmail.com>
2023-04-01 23:42:19 +0400

    Fix all descentant notes shown as child notes

--- components/NoteChildNotes.tsx
+++ components/NoteChildNotes.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { NoteLoader } from './NoteLoader';
-import { Event } from 'nostr-tools';
+import { Event, parseReferences } from 'nostr-tools';
 import { useQuery } from '@tanstack/react-query';
 import { getPublicRuntimeConfig } from '@/utils/getPublicRuntimeConfig';
+import { useMemo } from 'react';
+import { renderNoteContent } from '@/utils/renderNoteContent';
+import { getContentReferencedEvents } from '@/utils/getContentReferencedEvents';
+import { getThread } from '@/utils/getThread';
 
 export function NoteChildNotes({
 	id,
@@ -16,7 +20,30 @@ export function NoteChildNotes({
 		return fetch(childNotesUrl).then((response) => response.json())
 	});
 
-	const childNotes = childNotesQuery.data?.events || [];
+	const childNotes = useMemo(() => {
+		const childNoteEvents = childNotesQuery.data?.events || [];
+
+		return childNoteEvents.filter(childNoteEvent => {
+			const references = parseReferences(childNoteEvent);
+
+			const { contentTokens } = renderNoteContent({
+				content: childNoteEvent.content,
+				references,
+				pubkeyMetadatas: new Map(),
+			});
+
+			const contentReferencedEvents = getContentReferencedEvents(contentTokens);
+
+			const childThread = getThread(childNoteEvent, {
+				contentReferencedEvents,
+			});
+
+			return (
+				childThread.reply?.id === id
+				|| childThread.root?.id === id
+			);
+		});
+	}, [ id, childNotesQuery.data ]);
 
 	return (
 		<>
