18c43a29443bbe962fc1ab9986fe4fc99dda2b84
futpib <futpib@gmail.com>
2023-04-10 16:14:11 +0400

    Enable debug logs on front-end

--- utils/debugExtend.ts
+++ utils/debugExtend.ts
@@ -3,6 +3,14 @@ import debug from 'debug';
 
 const debugLog = debug('nostter');
 
+if (typeof window !== 'undefined') {
+	const namespaces = process.env.NEXT_PUBLIC_DEBUG;
+	if (namespaces) {
+		debug.enable(namespaces);
+		debug.log('debug enabled by NEXT_PUBLIC_DEBUG', namespaces);
+	}
+}
+
 export function debugExtend(...namespaces: string[]) {
 	invariant(namespaces.length > 0, 'No namespaces');
 
